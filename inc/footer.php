33a4a249d425d392c86f2679470a52b0d5e3db20
MeloZer0 <icaromelo011@gmail.com>
2023-06-20 22:59:35 +0100

    Added a simple footer

---
+++ inc/footer.php
@@ -0,0 +1,10 @@
+<div class="footer bg-light">
+  <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Início</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Social</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Sobre</a></li>
+    </ul>
+    <p class="text-center text-muted">© 2023 L'armoire de Melo</p>
+  </footer>
+</div>
--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php include('inc/header.php')
 ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <br><br><br>
+    <br><br><br>
+    <br><br><br>
+
+<?php include('inc/footer.php')
+?>
+
 </body>
 </html>
